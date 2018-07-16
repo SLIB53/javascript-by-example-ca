4bc3512b665d43a435cdd2c39fb64e75645d6152
Akil Krishnan <akil.krishnan.SLIB53@gmail.com>
2018-07-16 04:31:57 -0500

    Add values.js

---
+++ values/values.js
@@ -0,0 +1,22 @@
+const h = 'Hello';
+const w = "World";
+console.log(h);
+console.log(w);
+console.log(h + ", " + w + "!");
+
+console.log(1 + 1);
+console.log(2 + 3);
+
+console.log(true || false);
+console.log(!true);
+console.log(true ? "used string" : "unused string");
+
+
+console.log(null);
+console.log(undefined);
+
+const o = {
+    a: 0,
+    b: 1
+};
+console.log(o);

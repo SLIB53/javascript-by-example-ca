8a0aa6bf25ae8b1d2c07dce08f9cdcc716b6533b
Akil Krishnan <akil.krishnan.SLIB53@gmail.com>
2018-07-16 05:19:10 -0500

    Add array.js

---
+++ array/array.js
@@ -0,0 +1,17 @@
+const arr = [1, 2, 3];
+
+console.log(arr);
+
+
+let arr2 = [];
+arr2.push("a");
+arr2.push("b");
+
+console.log(arr2);
+console.log(arr2[999]);
+
+
+const arr3 = ["cat", "dog", "mouse"];
+const id = arr3.findIndex((v) => v == "dog");
+
+console.log(id);

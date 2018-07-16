8381d8c4f490512d41dd3077ecc2a70ab9edfef8
Akil Krishnan <akil.krishnan.SLIB53@gmail.com>
2018-07-16 04:52:54 -0500

    Add for.js

---
+++ for/for.js
@@ -0,0 +1,15 @@
+const species = {
+    human: 'homo sapien',
+    cat: 'felis silvestris'
+}
+
+for (const sk in species) {
+    console.log(species[sk]);
+}
+
+
+const faces = "☕️📜";
+
+for (const _f of faces) {
+    console.log(faces);
+}

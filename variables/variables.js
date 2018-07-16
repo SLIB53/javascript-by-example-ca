df26158849b1b8b5b203f31062011b4621b5ff20
Akil Krishnan <akil.krishnan.SLIB53@gmail.com>
2018-07-16 04:37:52 -0500

    Add variables.js

---
+++ variables/variables.js
@@ -0,0 +1,17 @@
+let a = 0, b = 0, c = 0;
+console.log(a);
+console.log(b);
+console.log(c);
+
+a = 1;
+b = 2;
+c = 3;
+console.log(a);
+console.log(b);
+console.log(c);
+
+
+const i = 10, j = 100, k = 1000;
+console.log(i);
+console.log(j);
+console.log(k);

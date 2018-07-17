29589a6bac9484ccf31ecc6a2335fe653cfdf0d5
Akil Krishnan <akil.krishnan.SLIB53@gmail.com>
2018-07-16 20:53:31 -0500

    Add functions.js

---
+++ functions/functions.js
@@ -0,0 +1,11 @@
+function scream (s) {
+    console.log(s.toUpperCase() + "!!!");
+}
+
+scream("there's always money in the banana stand");
+
+
+const yell = (s) => 
+    console.log(s.toUpperCase() + "!");
+
+yell("there's always money in the banana stand");
